393ec89869e6d1eb203bc71a36fb45eac2ed802f
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-17 23:35:42 +0200

    Added boilerplate for app directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+
+import FaviconSetup from '@components/FaviconSetup';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Vercel Postgres Demo with Prisma',
-  description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
-}
+    title: 'Opinion Rater',
+    description: 'Create and rate people\'s opinions'
+};
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+export default function RootLayout({ 
+    children 
+} : {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='en'>
+            <FaviconSetup />
+            <body className={`${inter.className}`}>
+                <main className='app'>
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
 }
