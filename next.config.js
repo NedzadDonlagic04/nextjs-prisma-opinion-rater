c74705b9d049cf61f15b8e9441d0b9bbdc973061
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-18 19:30:28 +0200

    Added redirect from base to login

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     domains: ['pbs.twimg.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
