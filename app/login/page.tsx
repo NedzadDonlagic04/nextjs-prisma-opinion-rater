9ac48dcb79d2d762f0e41e3e149e3c423bdc904f
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-20 19:49:05 +0200

    Moved forms into shared RHForm component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,5 @@
-import PasswordInputField from "@components/PasswordInputField";
+import RHForm from "@components/form/RHForm";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 
@@ -9,24 +7,14 @@ export default function Login() {
     return (
         <>
             <Typography variant="h3" sx={{ textAlign: 'center' }}>Login</Typography>
-            <form style={{ width: "100%", maxWidth: '450px' }}>
-                <Box 
-                    display='flex'
-                    flexDirection='column'
-                    sx={{ gap: 3 }}>
-                    <TextField required label="Username" name="username" />
-                    <PasswordInputField label="Password" name="password" />
-                    <Button
-                        variant="contained">
-                        Login
-                    </Button>
-                </Box>
-            </form>
-            <Box display="flex" justifyContent="flex-end" sx={{ width: 1 }}>
+            <RHForm type="login"/>
+            <Box 
+                display="flex" 
+                justifyContent="flex-end" 
+                sx={{ width: 1 }}>
                 <Link
                     style={{ fontSize: ".9rem", color: "royalblue", textAlign: 'center' }}
-                    href="/signup"
-                >
+                    href="/signup">
                     Don&apos;t have an account? Sign Up
                 </Link>
             </Box>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,27 +3,14 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
-import PasswordInputField from "@components/PasswordInputField";
+import PasswordInputField from "@components/form/PasswordInputField";
+import RHForm from "@components/form/RHForm";
 
 export default function SignUp() {
     return (
         <>
             <Typography variant="h3" sx={{ textAlign: 'center' }}>Sign Up</Typography>
-            <form style={{ width: "100%", maxWidth: '450px' }}>
-                <Box 
-                    display='flex'
-                    flexDirection='column'
-                    sx={{ gap: 3 }}>
-                    <TextField required label="Username" name="username"/>
-                    <TextField required label="Email" name="email"/>
-                    <PasswordInputField label="Password" name="password" />
-                    <PasswordInputField label="Confirm Password" name="confirm-password" />
-                    <Button
-                        variant="contained">
-                        Sign Up
-                    </Button>
-                </Box>
-            </form>
+            <RHForm type="signup" />
             <Box display="flex" justifyContent="flex-end" sx={{ width: 1 }}>
                 <Link
                     style={{ fontSize: ".9rem", color: "royalblue", textAlign: 'center' }}
