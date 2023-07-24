cf9b8eb95b83f1ce22ed5c12e600fb8e15b6912f
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-24 11:22:19 +0200

    Added error messages to schemas

--- lib/yup/schemas.tsx
+++ lib/yup/schemas.tsx
@@ -1,8 +1,14 @@
 import { object, string, ref } from 'yup';
 
 export const loginSchema = object({
-    username: string().min(3).max(20).required("Username must be between 3 and 20 characters"),
-    password: string().min(8).max(25).required("Password must be between 8 and 25 characters")
+    username: string()
+            .min(3, "Username must be at least 3 characters long")
+            .max(20, "Username must be shorter than 20 characters")
+            .required(),
+    password: string()
+            .min(8, "Password must be at least 8 characters long")
+            .max(25, "Password must be shorter than 25 characters")
+            .required()
 });
 
 export const signUpSchema = loginSchema.shape({
