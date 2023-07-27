61dba973cbc5f844a53954331968b601dbf4242e
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-27 17:30:24 +0200

    Added HTTP status constants to be used in signup / login API

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const HTTP_STATUS = {
+    SUCCESS: 200,                   // Everything works!
+    VALIDATION_ERROR: 403,          // Invalid format / validation error
+    USERNAME_ALREADY_EXISTS: 409,   // Conflict with an existing resource 
+};
