2b552976f9e4d822bbd9212a80f0b01773fd0f60
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-27 23:55:00 +0200

    Added file for exporting constants
    Currently has constants for HTTP status codes and
    local storage key value pair for user creation to
    show snackbar on login page

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,8 @@ export const HTTP_STATUS = {
     VALIDATION_ERROR: 403,          // Invalid format / validation error
     USERNAME_ALREADY_EXISTS: 409,   // Conflict with an existing resource 
 };
+
+export const DID_USER_GET_CREATED = {
+    KEY: 'user_created',
+    VALUE: 'success', 
+}
